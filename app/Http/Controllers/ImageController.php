9149a9379123ecae445906d2beae076bc4144c02
Rustam-Fozilov <fozilovrustam32@gmail.com>
2023-12-08 11:22:55 +0500

    linked storage folder

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
     public function store(Request $request)
     {
-        $image = $request->hasFile('image');
-        // $image_name = $image->getClientOriginalName();
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+        ]);
 
-        // $request['image']->move(public_path('images/books', $image_name . '.png'));
+        $image = $request->file('image');
 
-        return response(mb_convert_encoding($request['image'], 'UTF-8', 'UTF-8'));
+        $image_name = $image->getClientOriginalName();
+
+        Storage::fake('public');
+        Storage::disk('public')->put('images/books', $image);
+        
+        // $path = $image->storeAs('images/books', $image_name, 'public');
+        $image->move(public_path('images/books'), $image_name);
+        
+        return response($image);
     }
 }
